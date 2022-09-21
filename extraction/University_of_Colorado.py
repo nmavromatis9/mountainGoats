2e527fb60e98a770c28cf718792f13c0f9797568
Nick Mavromatis <nmavromatis9@gmail.com>
2022-09-21 16:36:55 +0000

    Preliminary script for U. of Colorado JSON extraction

---
+++ extraction/University_of_Colorado.py
@@ -0,0 +1,31 @@
+import json
+import csv
+
+#JSON script to extract data from U. of Colorado JSON sheet
+
+print("HELLO WORLD!")
+
+try:
+    f = open("./../JSON Files/uofcolorado.json", 'rb')
+except OSError:
+    print ("Could not open/read file uofcolorado.json")
+    sys.exit()
+    
+data=json.load(f)
+
+#for i in data['Associated_Codes'== '0001A']:
+    #print(i)
+
+l=len(data[0]['item'])
+#print("LEN=", l)
+
+cpt_codes=[10160,11010,21048,21557,32440,32480,32505,33218,33330,33361,33533,36415,45315,45380,45385,50040,59510,61518,71260,72125,72192,72195,72196,73000,74150,74250,80053,81210,81235,81272,82465,82803,82947,83874,84484,85025,86703,88230,99202,99203,99204,99205,99218,99219,99220,99281,99282,99283,99284,99285]
+
+#convert ints to strings with list comprehension
+cpt_codes = [str(x) for x in cpt_codes]
+
+for i in range(l):
+    for j in cpt_codes:   
+        if(data[0]['item'][i]['Associated_Codes'] == j):
+            print(data[0]['item'][i])
+            #print(i)
